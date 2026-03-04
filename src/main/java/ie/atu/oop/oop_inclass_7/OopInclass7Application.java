f62094dd63c0cbfd41cbeb2eb952f89d4105a318
Billy Lis-Murphy - STUDENT <g00465240@atu.ie>
2026-03-04 16:12:45 +0000

    Initial commit

---
+++ src/main/java/ie/atu/oop/oop_inclass_7/OopInclass7Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oop.oop_inclass_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopInclass7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopInclass7Application.class, args);
+    }
+
+}
